32e575ca6ff50c10d7d7d1ec4eae86d33d3206db
Exanite <exanite78012@gmail.com>
2022-12-26 18:37:35 -0500

    Change IPacketHandler.OnNetworkStarted to also be called when registering the packet handler to an already started Network

--- Network.cs
+++ Network.cs
@@ -65,6 +65,11 @@ namespace Exanite.Networking
         public void RegisterPacketHandler(IPacketHandler handler)
         {
             packetHandlers.Add(handler.HandlerId, handler);
+
+            if (Status == LocalConnectionStatus.Started)
+            {
+                handler.OnNetworkStarted(this);
+            }
         }
 
         public void UnregisterPacketHandler(IPacketHandler handler)
