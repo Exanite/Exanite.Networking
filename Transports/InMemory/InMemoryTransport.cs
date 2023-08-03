5f5f990209ce2ec44a05088a8aa4757b6324b502
Exanite <exanite78012@gmail.com>
2023-08-02 22:09:17 -0400

    Add initial implementation of InMemoryTransport (untested)

--- Transports/InMemory/InMemoryTransport.cs
+++ Transports/InMemory/InMemoryTransport.cs
@@ -1,60 +1,141 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
+using Exanite.Core.Collections;
 using Exanite.Core.Events;
+using UniDi;
 using UnityEngine;
 
 namespace Exanite.Networking.Transports.InMemory
 {
-    public class InMemoryTransport : MonoBehaviour, ITransport
+    public abstract class InMemoryTransport : MonoBehaviour, ITransport
     {
-        public LocalConnectionStatus Status { get; }
+        private TwoWayDictionary<int, InMemoryTransport> connections;
+
+        private Queue<TransportConnectionStatusEventArgs> connectionEventQueue;
+        private Queue<TransportDataReceivedEventArgs> dataEventQueue;
+
+        private int nextConnectionId = 0;
+
+        [Inject] protected InMemoryTransportSettings Settings { get; }
+
+        public LocalConnectionStatus Status { get; protected set; }
+
         public event EventHandler<ITransport, TransportDataReceivedEventArgs> DataReceived;
         public event EventHandler<ITransport, TransportConnectionStatusEventArgs> ConnectionStatus;
 
-        public void Tick()
+        private void Awake()
         {
-            throw new NotImplementedException();
+            connections = new();
+            connectionEventQueue = new();
+            dataEventQueue = new();
         }
 
-        public UniTask StartConnection()
+        private void OnDestroy()
         {
-            throw new NotImplementedException();
+            StopConnection(false);
         }
+
+        public void Tick()
+        {
+            PushEvents();
+        }
+
+        public abstract UniTask StartConnection();
 
         public void StopConnection()
         {
-            throw new NotImplementedException();
+            StopConnection(true);
+        }
+
+        protected virtual void StopConnection(bool handleEvents)
+        {
+            try
+            {
+                if (handleEvents)
+                {
+                    PushEvents();
+                }
+            }
+            finally
+            {
+                connections.Clear();
+
+                Status = LocalConnectionStatus.Stopped;
+            }
         }
 
         public RemoteConnectionStatus GetConnectionStatus(int connectionId)
         {
-            throw new NotImplementedException();
+            return connections.ContainsKey(connectionId) ? RemoteConnectionStatus.Started : RemoteConnectionStatus.Stopped;
         }
 
         public void DisconnectConnection(int connectionId)
         {
-            throw new NotImplementedException();
+            if (connections.TryGetValue(connectionId, out var remoteTransport))
+            {
+                connections.Remove(connectionId);
+                connectionEventQueue.Enqueue(new TransportConnectionStatusEventArgs(connectionId, RemoteConnectionStatus.Stopped));
+
+                if (remoteTransport.connections.Inverse.TryGetValue(this, out var remoteId))
+                {
+                    remoteTransport.connections.Remove(remoteId);
+                    remoteTransport.connectionEventQueue.Enqueue(new TransportConnectionStatusEventArgs(remoteId, RemoteConnectionStatus.Stopped));
+                }
+            }
         }
 
         public void SendData(int connectionId, ArraySegment<byte> data, SendType sendType)
         {
-            throw new NotImplementedException();
+            if (!connections.TryGetValue(connectionId, out var remoteTransport))
+            {
+                throw new NetworkException("Attempted to send data to invalid connection.");
+            }
+
+            if (!remoteTransport.connections.Inverse.TryGetValue(this, out var remoteId))
+            {
+                throw new NetworkException("Missing connection on remote. Local and remote connection lists are mismatched.");
+            }
+
+            // Currently always sends reliably.
+            // Todo Pool arrays.
+            remoteTransport.dataEventQueue.Enqueue(new TransportDataReceivedEventArgs(remoteId, data.ToArray(), sendType));
         }
-    }
 
-    public class InMemoryTransportClient : InMemoryTransport, ITransportClient {}
-
-    public class InMemoryTransportServer : InMemoryTransport, ITransportServer {}
-
-    public class InMemoryTransportSettings : MonoBehaviour
-    {
-        [Header("Configuration")]
-        [SerializeField] private int virtualAddress = 0;
-
-        public int VirtualAddress
+        protected void PushEvents()
         {
-            get => virtualAddress;
-            set => virtualAddress = value;
+            while (connectionEventQueue.TryDequeue(out var e))
+            {
+                ConnectionStatus?.Invoke(this, e);
+            }
+
+            while (dataEventQueue.TryDequeue(out var e))
+            {
+                DataReceived?.Invoke(this, e);
+            }
+        }
+
+        /// <summary>
+        /// Only called from <see cref="InMemoryTransportClient"/>.
+        /// </summary>
+        /// <remarks>
+        /// It's bad practice to refer to inheriting classes in base class, but it's the easiest way to keep the API mostly hidden.
+        /// </remarks>
+        public void OnClientConnected(InMemoryTransportClient client)
+        {
+            if (connections.Inverse.ContainsKey(client) || client.connections.Inverse.ContainsKey(this))
+            {
+                throw new NetworkException("Already connected.");
+            }
+
+            connections.Add(nextConnectionId, client);
+            connectionEventQueue.Enqueue(new TransportConnectionStatusEventArgs(nextConnectionId, RemoteConnectionStatus.Started));
+            nextConnectionId++;
+
+            client.connections.Add(client.nextConnectionId, this);
+            client.connectionEventQueue.Enqueue(new TransportConnectionStatusEventArgs(client.nextConnectionId, RemoteConnectionStatus.Started));
+            client.nextConnectionId++;
         }
     }
 }
